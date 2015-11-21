1d7e9c5e7315161883aa7b01ce7fe4a4d8d7e6f3
Jay <jay.wang.scorpion@qq.com>
2015-11-21 14:43:54 +0800

    spring mvc

---
+++ src/main/java/com/boot/controller/TestController.java
@@ -0,0 +1,14 @@
+package com.boot.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+	@RequestMapping(value="test")
+	public String test(){
+		return "return from controller";
+	}
+	
+}
